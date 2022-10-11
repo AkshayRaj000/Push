806445be19d88323d5e612619e798432ea2bab7d
Akshay <akshay2003994@st.jmi.ac.in>
2022-10-11 15:46:49 +0530

    pushing my code to github

---
+++ 16_MongoDB/connetmongodb.py
@@ -0,0 +1,16 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://mongodbakshay:Akshay1000#@cluster0.0206nhg.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
+
+
+dict = {
+    'First name' : 'Akshay',
+    'Last name' : 'Raj'
+}
+
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(dict)
